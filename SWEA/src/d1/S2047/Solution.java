6697532143596c9d02496a0c80d39a8e87c74d56
hyunhee <wjdgusgml997@naver.com>
2020-12-01 11:56:13 +0900

    [+]UPDATE d1/S2047

--- SWEA/src/d1/S2047/Solution.java
+++ SWEA/src/d1/S2047/Solution.java
@@ -1,8 +1,15 @@
 package d1.S2047;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		System.out.println(input.toUpperCase());
 		
 	}
 
