2568caf5578b156491bc191f54a53e406b061027
hyunhee <wjdgusgml997@naver.com>
2021-02-15 18:27:59 +0900

    [+]ADD BOJ/bronze1/B11050

---
+++ BOJ/src/bronze1/B11050/Main.java
@@ -0,0 +1,28 @@
+package bronze1.B11050;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(str.nextToken());
+		int K = Integer.parseInt(str.nextToken());
+		System.out.println((factorial(N) / factorial(K)) / factorial(N - K));
+	}
+
+	private static int factorial(int n) {
+		if (n == 1) {
+			return 1;
+		}
+		if (n == 0) {
+			return 1;
+		}
+		return n * factorial(n - 1);
+	}
+
+}
