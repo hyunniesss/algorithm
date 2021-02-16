26271c29538ff4cb0821eda1716f67e98052de89
hyunhee <wjdgusgml997@naver.com>
2021-02-16 14:02:33 +0900

    [+]ADD BOJ/bronze2/B16968

---
+++ BOJ/src/bronze2/B16968/Main.java
@@ -0,0 +1,45 @@
+package bronze2.B16968;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static char[] input;
+	static int answer;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		input = br.readLine().toCharArray();
+		permutation(' ', 0);
+		System.out.println(answer);
+	}
+
+	private static void permutation(char c, int cnt) {
+		if (cnt == input.length) {
+			answer++;
+			return;
+		}
+
+		switch (input[cnt]) {
+		case 'c':
+			for (int i = 0; i < 26; i++) {
+				if (c == (char) (i + 'a')) {
+					continue;
+				}
+				permutation((char) (i + 'a'), cnt + 1);
+			}
+			break;
+		case 'd':
+			for (int i = 0; i < 10; i++) {
+				if (c == (char) (i + '0')) {
+					continue;
+				}
+				permutation((char) (i + '0'), cnt + 1);
+			}
+			break;
+		}
+	}
+
+}
