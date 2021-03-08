af2cf3752d1833d47ded130b1f48ff4b48839f71
hyunhee <wjdgusgml997@naver.com>
2021-03-08 12:47:07 +0900

    [+]ADD gold5/B14226 and silver2/B16928

---
+++ BOJ/src/bronze1/B2750_Main.java
@@ -0,0 +1,30 @@
+package bronze1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B2750_Main {
+
+	static int N;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		for (int n = 0; n < N; n++) {
+			arr[n] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+
+		for (int a : arr) {
+			sb.append(a).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
