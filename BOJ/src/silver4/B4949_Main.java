443307bf1cac0c12a61a9ff34984580a5762c02b
hyunhee <wjdgusgml997@naver.com>
2021-05-02 20:41:30 +0900

    [+]UPD silver4/B4949 and gold5/B17144_incomplete

--- BOJ/src/silver4/B4949_Main.java
+++ BOJ/src/silver4/B4949_Main.java
@@ -1,10 +1,10 @@
-package silver4.B4949;
+package silver4;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main {
+public class B4949_Main {
 
 	static StringBuilder answer = new StringBuilder();
 	static Stack<Character> stack = new Stack<>();
