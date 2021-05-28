7635159171ce6a63110869ce6596dd949285289a
hyunhee <wjdgusgml997@naver.com>
2021-05-29 01:01:41 +0900

    [+]ADD C++ 디버깅

---
+++ c++/jungol/Language_Coder/디버깅/117.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b, c;
+    cin >> a >> b >> c;
+    printf("sum %d\navg %d", (int)a + (int)b + (int)c, (int)(a + b + c) / 3);
+}
---
+++ c++/jungol/Language_Coder/디버깅/527.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    printf("%d %.2f", a / b, (double)a / b);
+}
